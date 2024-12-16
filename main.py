cec3539fffcd2fdee5202e80b4b6c17e291f1a26
John Matthew Saraga Arroyo <johnmatthewarroyo2@gmail.com>
2024-12-16 23:29:47 +0800

    add main.py from branch-1

---
+++ main.py
@@ -0,0 +1,29 @@
+from clubtech import arroyo, nolluda
+import time
+import os
+
+while True:
+    print("============")
+    print("MAIN MENU (CLUBTECH)")
+    print("============")
+    print("1. John Matthew Arroyo")
+    print("2. Franco Alfoson Lazaro")
+    print("3. John Carlo Nolluda")
+    print("4. Exit")
+    print("==========================")
+    user_choice = int(input("Enter your choice: "))
+
+    match user_choice:
+        case 1:
+            os.system('cls')
+            arroyo.arroyo_object()
+        case 2:
+            os.system('cls')
+            nolluda.nolluda_object()
+        case 4:
+            print("Bye Bye!")
+            break
+        case _:
+            print("Incorrect choice (Pick 1 - 4 only)")
+            
+
